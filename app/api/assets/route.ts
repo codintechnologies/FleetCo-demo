1192aed83519602be969c16034352554a786d12a
codintechnologies <codintechnologies@gmail.com>
2025-11-10 16:11:55 +0300

    Delete app directory

--- app/api/assets/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { fetchVehicles } from '@/lib/services';
-
-export async function GET() {
-	const data = await fetchVehicles();
-	return NextResponse.json(data);
-}
-
--- app/api/trips/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { fetchTrips } from '@/lib/services';
-
-export async function GET() {
-	const data = await fetchTrips();
-	return NextResponse.json(data);
-}
-
